bdd269e20d17c9d3a6702f5ccff3699957deb215
hungnv132 <hungnv132@gmail.com>
2017-11-18 07:37:57 +0700

    modify db statements

--- shell/init_db.sh
+++ shell/init_db.sh
@@ -3,8 +3,8 @@
 username='root'
 
 sudo mysql -u${username} -p -e \
-    "
-     CREATE DATABASE IF NOT EXISTS zero_db;
+    "DROP DATABASE IF EXISTS zero_db;
+     CREATE DATABASE zero_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;;
      CREATE USER IF NOT EXISTS'admin'@'%' IDENTIFIED BY '123456';
      GRANT ALL PRIVILEGES ON zero_db.* TO 'admin'@'%' WITH GRANT OPTION;
      SHOW DATABASES;
