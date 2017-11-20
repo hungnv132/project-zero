805d7d7920571ac4e6f8fe8f9464f42ca9eecc8d
hungnv132 <hungnv132@gmail.com>
2017-11-20 17:58:25 +0700

    add abstract class 'TrackingUser'

--- zero/core/models.py
+++ zero/core/models.py
@@ -1,9 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class TrackingTime(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class TrackingUser(models.Model):
+    created_by = models.ForeignKey(User, related_name="%(app_label)s_%(class)s_created",
+                                   blank=True, null=True)
+    modified_by = models.ForeignKey(User, related_name="%(app_label)s_%(class)s_modified",
+                                    blank=True, null=True)
 
     class Meta:
         abstract = True
