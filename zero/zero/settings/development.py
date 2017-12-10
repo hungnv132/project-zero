9c1e2c227be3aac8c2046ec98fc9612c06308117
hungnv132 <hungnv132@gmail.com>
2017-12-10 17:54:26 +0700

    change config of uwsgi

---
+++ zero/zero/settings/development.py
@@ -0,0 +1,67 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+USE_L10N = False
+DATE_FORMAT = 'd/m/Y'
+DATETIME_FORMAT = 'd/m/Y H:i:s'
+
+TIME_ZONE = 'Asia/Ho_Chi_Minh'
+
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'hungnv132.net']
+
+CKEDITOR_UPLOAD_PATH = 'static/images/'
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'compressor',
+    'require',
+    'ckeditor',
+    'ckeditor_uploader',
+    'stock',
+    'blog'
+]
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'zero_db',
+        'USER': 'admin',
+        'PASSWORD': 'gacon132456',
+        'HOST': '127.0.0.1',
+        'PORT': 3306,
+    }
+}
+
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    # other finders..
+    'compressor.finders.CompressorFinder',
+)
+COMPRESS_ENABLED = True
+
+COMPRESS_PRECOMPILERS = (
+    ('text/x-scss', 'django_libsass.SassCompiler'),
+)
+
+# settings CKEditor
+
+CKEDITOR_IMAGE_BACKEND = 'pillow'
+CKEDITOR_RESTRICT_BY_DATE = False
+CKEDITOR_UPLOAD_PATH = 'blog/'
+CKEDITOR_FILENAME_GENERATOR = 'core.utils.get_image_filename'
+CKEDITOR_CONFIGS = {
+    'default': {
+        'height': 300,
+        'width': 1200,
+    },
+}
--- zero/zero/wsgi.py
+++ zero/zero/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "zero.settings.local")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "zero.settings")
 
 application = get_wsgi_application()
