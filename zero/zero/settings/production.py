81348e57ebf313a3da5fe5f150348a9158f903c0
hungnv132 <hungnv132@gmail.com>
2017-12-10 18:18:32 +0700

    refactor code

--- zero/zero/settings/production.py
+++ zero/zero/settings/production.py
@@ -34,7 +34,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'zero_db',
         'USER': 'admin',
-        'PASSWORD': 'Gacon13245678()',
+        'PASSWORD': '123456',
         'HOST': '127.0.0.1',
         'PORT': 3306,
     }
