588275e265089b985029a803e40d70c4671f3648
hungnv132 <hungnv132@gmail.com>
2017-12-14 21:31:36 +0700

    update script deploy the project

--- shell/deploy_project.sh
+++ shell/deploy_project.sh
@@ -2,5 +2,9 @@ git stash
 git pull origin develop
 git stash pop
 
+cd zero
+export DJANGO_SETTINGS_MODULE="zero.settings.production"
+python manage.py collectstatic --noinput
+
 sudo service nginx restart
 sudo systemctl restart uwsgi.service
