58f57d2ae3bbfa19411edffd55ea4126f7fa2b78
hungnv132 <hungnv132@gmail.com>
2017-12-14 20:42:20 +0700

    create shell script tool

---
+++ shell/copy_nginx_conf.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+sites_available=/etc/nginx/sites-available
+sites_enabled=/etc/nginx/sites-enabled
+project=/home/hungnv132/project-zero
+
+sudo rm $sites_available/nginx_zero
+sudo rm $sites_enabled/nginx_zero
+sudo cp $project/config/nginx_zero $sites_available
+sudo ln -s $sites_available/nginx_zero $sites_enabled
