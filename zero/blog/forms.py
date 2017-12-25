746fec30c411007cc0b329886b1dffb18cfb46d6
hungnv132 <hungnv132@gmail.com>
2017-12-25 22:09:08 +0700

    use simple textarea for 'intro' input

--- zero/blog/forms.py
+++ zero/blog/forms.py
@@ -1,13 +1,13 @@
 from django import forms
+from django.forms.widgets import Textarea
 from blog.models import Post
-from ckeditor.widgets import CKEditorWidget
 from ckeditor_uploader.widgets import CKEditorUploadingWidget
 
 
 class PostForm(forms.ModelForm):
     title = forms.CharField(widget=forms.TextInput(attrs={'size': 60}))
     slug = forms.CharField(widget=forms.TextInput(attrs={'size': 60}))
-    intro = forms.CharField(widget=CKEditorWidget(attrs={'rows': 10}))
+    intro = forms.CharField(widget=Textarea(attrs={'rows': 6, 'cols': 125}))
     content = forms.CharField(widget=CKEditorUploadingWidget())
 
     class Meta:
