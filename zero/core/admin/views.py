f36aa86c7ff768a0d76c66e1e4718515fc3bc74b
hungnv132 <hungnv132@gmail.com>
2017-12-31 16:24:13 +0700

    create DataBox to pull stock data

--- zero/core/admin/views.py
+++ zero/core/admin/views.py
@@ -1,6 +1,7 @@
 from django.core.urlresolvers import reverse
 from django.views.generic import TemplateView
 from django.contrib import admin
+from stock.services import Quote
 
 
 def custom_admin_view_urls():
@@ -27,3 +28,16 @@ class CustomAdminView(TemplateView):
 class StockDailyView(CustomAdminView):
     template_name = 'admin/stock_daily.html'
     title = site_header = 'Stock Daily'
+
+    def get_context_data(self, **kwargs):
+        context = super(CustomAdminView, self).get_context_data(**kwargs)
+        symbols = ['VNM', 'VCB', 'HPG', 'PVS']
+        quotes = list()
+        for symbol in symbols:
+            quote = Quote(symbol)
+            quote.update_prices_today()
+            quotes.append(quote)
+        context.update({
+            'quotes': quotes,
+        })
+        return context
