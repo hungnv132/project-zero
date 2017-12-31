3c68260501b39c12e47d38eb179c13d243aeda31
hungnv132 <hungnv132@gmail.com>
2017-12-31 16:22:59 +0700

    add template for stock daily

---
+++ zero/static/js/trading_daily.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('#interesting-quotes').dynatable({
+        features: {
+            paginate: false,
+            recordCount: true,
+            sorting: false
+        },
+    });
+});
