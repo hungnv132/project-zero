e9f1f61c44f5378c36985761dfc45c1eee989946
hungnv132 <hungnv132@gmail.com>
2018-02-04 18:39:34 +0700

    update deploy project in shell

--- bin/deploy_project.sh
+++ bin/deploy_project.sh
@@ -5,7 +5,10 @@ git stash pop
 source /home/ubuntu/project-zero/env/bin/activate
 cd /home/ubuntu/project-zero/zero
 export DJANGO_SETTINGS_MODULE="zero.settings.production"
+
+pip install -r ../requirements.txt
 python manage.py collectstatic --noinput
+python manage.py migrate
 
 sudo service nginx restart
 sudo systemctl restart uwsgi.service
